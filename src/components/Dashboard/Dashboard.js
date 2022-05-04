22f7ebbcedb3dcc4aa86bfd3cdc5476fd67c175a
aldrich2002 <lrichxo@gmail.com>
2022-05-04 22:39:26 +0530

    added tables

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,38 +4,106 @@ import { useState } from 'react';
 import {useEffect} from 'react';
 import Donut from '../Donut/Donut';
 import Sentiment from '../Sentiment/Sentiment.js'
+import './Dashboard.css'
+
 
 function Dashboard() {
     let location = useLocation();
     const [data, setData] = useState();
-    var top_pos;
-    var top_neg;
-    var avg_comp;
-    var full_data;
-    var title;
+    var top_posc=[];
+    var top_negc;
+    const[com,comset]=useState()
+    var full_datac=[];
+    var full_datae=[];
+    var title=[];
     var donut;
+    let vals = [];
+  
     useEffect(()=>{
         setData(location.state);
+        console.log(data)
+     
         
        
 
-    }, [])
+    }, [data])
     
 if(data){
+
     if(data.platform == 'youtube'){
+        top_negc=Object.values(data.top_neg.Comments)
+        top_posc=Object.values(data.top_pos.Comments)
+        full_datac=Object.values(data.full_data.Comments)
+        full_datae=Object.values(data.full_data.comp_score)
+        for(var j in full_datae){
+            if(full_datae[j] ==='neg'){
+                full_datae[j]='😞';
+    
+            }
+            else if(full_datae[j] ==='pos'){
+                full_datae[j]='🙂';
+            }
+            else {
+                full_datae[j]='😐';
+            }
+        } 
         
-        console.log(data)
+        
+       
         return (
             <div>
+                <h2>
+                    {data.title}
+               </h2>
                 <div>
                     <Donut pts={data.donut}/> 
                </div>
                <div>
                     <Sentiment avg={data.avg_comp}/>
                </div>
-               <div>
-                   Title is {data.title}
+               <div id='top neg'>
+                   
+                    <h1> Top ten negative Comments </h1>
+                    {top_negc.map(name => (  
+                        <li>  
+                            {name}  
+                        </li>  
+                      ))}    
+
+               
+    
                </div>
+               <div id='top pos'>
+                   
+                    <h1> Top ten Postive Comments </h1>
+                    {top_posc.map(name => (  
+                        <li>  
+                            {name}  
+                        </li>  
+                      ))}    
+
+    
+               </div>
+               <div id='full_data'>
+                   
+                   <h1> ALl Comments </h1>
+                   <table>
+                   <tr>
+                    <td>Comments</td>
+                    <td>emotion</td>
+                    
+                  </tr>
+                   {full_datac.map((name,i) => (
+                       <tr>
+                       <td>{name}</td>
+                       <td>{full_datae[i]}</td>
+                     </tr>  
+                       
+                     ))}    
+                  </table>
+   
+              </div>
+               
             </div>
         )
         
@@ -43,7 +111,23 @@ if(data){
 
     else if(data.platform == 'twitter')
     {
-        return (
+       
+        full_datac=Object.values(data.full_data.Tweet)
+        full_datae=Object.values(data.full_data.comp_score)
+        for(var j in full_datae){
+            if(full_datae[j] ==='neg'){
+                full_datae[j]='😞';
+    
+            }
+            else if(full_datae[j] ==='pos'){
+                full_datae[j]='🙂';
+            }
+            else {
+                full_datae[j]='😐';
+            }
+        } 
+        console.log(data)
+                return (
             <div>
             <div>
                 <Donut pts={data.donut}/> 
@@ -51,6 +135,28 @@ if(data){
            <div>
                 <Sentiment avg={data.avg_comp}/>
            </div>
+          
+           <div id='full_data'>
+                   
+                   <h1> ALl Comments </h1>
+                   <table>
+                   <tr>
+                    <td>Comments</td>
+                    <td>emotion</td>
+                    
+                  </tr>
+                   {full_datac.map((name,i) => (
+                       <tr>
+                       <td>{name}</td>
+                       <td>{full_datae[i]}</td>
+                     </tr>  
+                       
+                     ))}    
+                  </table>
+   
+              </div>
+
+           
            
         </div>
         )
